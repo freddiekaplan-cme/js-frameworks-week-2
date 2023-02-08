cc16b4996121fa7379d9e8f4e91693459a40d802
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-02-08 13:03:24 +0100

    Added html code and css file

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ argumentParser.option('--date')
 argumentParser.parse();
 const dateArgument = argumentParser.args[0];
 
+//2023, 2, 7
+let yourDate = '';
+let beforeOrAfter = '';
+let timeSymbol ='';
+
+function dateFormatAdjust() {
+	yourDate = dateArgument.replace(/-/g, ", ");
+}
+dateFormatAdjust();
+
 const asyncExec = util.promisify(exec);
 const first = ('Freddie');
 const last = ('Kaplan');
@@ -20,29 +30,25 @@ const compareDate = format(new Date(yourDate), 'yyyy-MM-dd')
 const startOfCourse = new Date(2023, 0, 31)
 const daysSinceStart = formatDistanceToNow(startOfCourse)
 const todaysDate = format(new Date(), 'yyyy-MM-dd')
-const message = chalk.bgGreen('You entered date ' + argumentParser.args[0] + '. Take a look in index.md.')
+const message = chalk.bgGreen('You entered date ' + argumentParser.args[0] + '. Take a look at index.html.')
 const prettyNpmAndNode = process.env.npm_config_user_agent.slice(0, 23);
 const prettyGit = stdout.slice(0, 18);
 //const hebrewCal = Temporal.Calendar.from('hebrew');
 //const hebrewDate = Temporal.Now.plainDateTime('hebrew');
-let yourDate = '2023, 2, 7';
-let beforeOrAfter = 'before';
 Date.prototype.toTemporalInstant = toTemporalInstant;
-
-function dateFormatAdjust() {
-	yourDate = dateArgument.replace(/-/g, ", ");
-}
-dateFormatAdjust();
 
 function dateComparison() {
 	if (compareDate < todaysDate) {
 		beforeOrAfter = 'before';
+		timeSymbol = '⌛';
 	}
 	else if (compareDate > todaysDate) {
 		beforeOrAfter = 'after';
+		timeSymbol = '🚀';
 	}
 	else if (compareDate === todaysDate) {
 		beforeOrAfter = 'exactly';
+		timeSymbol = '⏰';
 	}
 }
 dateComparison();
@@ -50,17 +56,49 @@ dateComparison();
 // cal = Temporal.Calendar.from('hebrew')
 // const today = date.withCalendar('hebrew');
 // console.log(today.toString());
+//Today's date in the Hebrew calendar: ${hebrewDate}
 
 console.log(message);
 
 const fileContent = `
-Name: ${name}
-Npm & node: ${prettyNpmAndNode}
-Git version: ${prettyGit}
-Days since start of course: ${daysSinceStart}
-Today's date: ${todaysDate}
-Today's date in the Hebrew calendar: ${hebrewDate}
-Your entered date ${compareDate} is ${beforeOrAfter} today's date.
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="style.css" />
+    	<title>Week 2</title>
+	</head>
+	<body>
+		<header class="header">
+			<h1>Week 2 ${timeSymbol}</h1>
+    	</header>
+    	<main class="content">
+			<div class="content-item">
+				Npm & node: ${prettyNpmAndNode}
+			</div>
+			<div class="content-item">
+				Git version: ${prettyGit}
+			</div>
+			<div class="content-item">
+				Days since start of course: ${daysSinceStart}
+			</div>
+			<div class="content-item">
+				Today's date: ${todaysDate}
+			</div>
+			<div class="content-item">
+				Your entered date ${compareDate} is ${beforeOrAfter} today's date.
+			</div>
+    	</main>
+		<footer class="footer">
+			Made by ${name}
+  		</footer>
+</body>
+</html>
 `;
 
-await fs.promises.writeFile("index.md", fileContent);
+console.log(yourDate);
+console.log(compareDate);
+console.log(todaysDate);
+
+await fs.promises.writeFile("index.html", fileContent);
