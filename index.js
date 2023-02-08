e7b02d81fe8f6527eb06cbea6721bdf06b08db7c
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-02-08 16:28:45 +0100

    Added Hebrew year with Temporal.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import gitVersion from './src/gitVersion.js'
 import chalk from 'chalk';
 import fs from 'fs';
 import { format, formatDistanceToNow } from 'date-fns';
-import { Temporal, Intl, toTemporalInstant } from '@js-temporal/polyfill';
+import { Temporal, toTemporalInstant } from '@js-temporal/polyfill';
 import { Command } from 'commander';
+
+Date.prototype.toTemporalInstant = toTemporalInstant;
 
 const argumentParser = new Command();
 argumentParser.option('--date')
@@ -14,10 +16,9 @@ let yourDate = '';
 let beforeOrAfter = '';
 let timeSymbol ='';
 
-function dateFormatAdjust() {
+(() => {
 	yourDate = dateArgument.replace(/-/g, ", ");
-}
-dateFormatAdjust();
+})();
 
 const first = ('Freddie');
 const last = ('Kaplan');
@@ -29,10 +30,8 @@ const todaysDate = format(new Date(), 'yyyy-MM-dd HH:mm')
 const message = chalk.bgGreen('You entered date ' + argumentParser.args[0] + '. Take a look at index.html.')
 const prettyNpmAndNode = process.env.npm_config_user_agent.slice(0, 23);
 const prettyGit = await gitVersion();
-
-//const hebrewCal = Temporal.Calendar.from('hebrew');
-//const hebrewDate = Temporal.Now.plainDateTime('hebrew');
-Date.prototype.toTemporalInstant = toTemporalInstant;
+const todayDateString = compareDate.toString();
+const hebrewDate = Temporal.PlainDate.from(todayDateString).withCalendar('hebrew');
 
 function dateComparison() {
 	if (compareDate < todaysDate) {
@@ -50,11 +49,6 @@ function dateComparison() {
 }
 dateComparison();
 
-// cal = Temporal.Calendar.from('hebrew')
-// const today = date.withCalendar('hebrew');
-// console.log(today.toString());
-//Today's date in the Hebrew calendar: ${hebrewDate}
-
 const fileContent = `
 <!DOCTYPE html>
 <html lang="en">
@@ -70,19 +64,19 @@ const fileContent = `
     	</header>
     	<main class="content">
 			<div class="content-item">
-				Npm & node: ${prettyNpmAndNode}
+				Npm & Node version: <span class="results">${prettyNpmAndNode}</span>
 			</div>
 			<div class="content-item">
-			Git version: ${prettyGit}
+			Git version: <span class="results">${prettyGit}</span>
 			</div>
 			<div class="content-item">
-				Days since start of course: ${daysSinceStart}
+				Days since start of course: <span class="results">${daysSinceStart}</span>
 			</div>
 			<div class="content-item">
-				Today's date and time: ${todaysDate}
+				Today's date and time: <span class="results">${todaysDate}</span>
 			</div>
 			<div class="content-item">
-				The entered date ${compareDate} is ${beforeOrAfter} today's date.
+				The entered date <span class="results">${compareDate}</span> is <span class="results">${beforeOrAfter}</span> today's date, and is in the year of <span class="results">${hebrewDate.year}</span> of the Hebrew calendar.
 			</div>
     	</main>
 		<footer class="footer">
