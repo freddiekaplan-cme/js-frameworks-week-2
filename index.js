e78d6cf6e73fd0a361484e87c415bb3539fe37b9
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-02-08 11:28:24 +0100

    Add date as argument.

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import chalk from 'chalk';
 import {exec} from 'child_process';
 import fs from 'fs';
 import util from 'util';
-import { compareAsc, format, formatDistanceToNow } from 'date-fns'
+import { compareAsc, format, formatDistanceToNow, isAfter, isBefore, parse } from 'date-fns';
 
-//ENTER A DATE YYYY, MM, DD
-let yourDate = '2023, 2, 7';
+import { Temporal, Intl, toTemporalInstant } from '@js-temporal/polyfill';
+Date.prototype.toTemporalInstant = toTemporalInstant;
+
+import { Command } from 'commander';
+const argumentParser = new Command();
 
 const asyncExec = util.promisify(exec);
 const first = ('Freddie');
@@ -16,8 +19,25 @@ const compareDate = format(new Date(yourDate), 'yyyy-MM-dd')
 const startOfCourse = new Date(2023, 0, 31)
 const daysSinceStart = formatDistanceToNow(startOfCourse)
 const todaysDate = format(new Date(), 'yyyy-MM-dd')
-const message = chalk.bgGreen('You entered date ' + yourDate + '. Take a look in index.md.')
+const message = chalk.bgGreen('You entered date ' + argumentParser.args[0] + '. Take a look in index.md.')
+const prettyNpmAndNode = process.env.npm_config_user_agent.slice(0, 23);
+const prettyGit = stdout.slice(0, 18);
+//const hebrewCal = Temporal.Calendar.from('hebrew');
+//const hebrewDate = Temporal.Now.plainDateTime('hebrew');
 let beforeOrAfter = 'before';
+
+argumentParser.option('--date')
+argumentParser.parse();
+const dateArgument = argumentParser.args[0];
+
+//ENTER A DATE YYYY, MM, DD
+let yourDate = '2023, 2, 7';
+
+function dateFormatAdjust() {
+	yourDate = dateArgument.replace(/-/g, ", ");
+}
+dateFormatAdjust();
+
 console.log(message);
 
 function dateComparison() {
@@ -33,13 +53,18 @@ function dateComparison() {
 }
 dateComparison();
 
-const data = `
+// cal = Temporal.Calendar.from('hebrew')
+// const today = date.withCalendar('hebrew');
+// console.log(today.toString());
+//Today's date in the Hebrew calendar: ${hebrewDate}
+
+const fileContent = `
 Name: ${name}
-Npm & node: ${process.env.npm_config_user_agent}
-Git version: ${stdout}
+Npm & node: ${prettyNpmAndNode}
+Git version: ${prettyGit}
 Days since start of course: ${daysSinceStart}
 Today's date: ${todaysDate}
 Your entered date ${compareDate} is ${beforeOrAfter} today's date.
 `;
 
-await fs.promises.writeFile("index.md", data);
+await fs.promises.writeFile("index.md", fileContent);
--- index.md
+++ index.md
@@ -1,9 +1,7 @@
 
 Name: Freddie Kaplan
-Npm & node: npm/9.4.0 node/v18.13.0 win32 x64 workspaces/false
-Git version: git version 2.36.0.windows.1
-
+Npm & node: npm/9.4.0 node/v18.13.0
+Git version: git version 2.36.0
 Days since start of course: 8 days
-Today's date: 2023-02-07
-Your entered date 2023-02-07 is exactly today's date.
-
+Today's date: 2023-02-08
+Your entered date 2014-01-01 is before today's date.
